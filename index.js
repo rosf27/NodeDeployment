24119c6266a7d487e0103bb00cfcc5d750e1d3a9
rsanchez27 <rsanchezf@une.net.co>
2021-10-27 00:35:30 -0500

    Primer commit

--- index.js
+++ index.js
@@ -4,14 +4,28 @@ const mongoose = require('mongoose')
 const app = require('./app')
 const config = require('./config')
 
-mongoose.connect(config.db, (err, res) => {
+//variables para puerto local y para heroku
+//Leer localhost de variables y puerto
+const host = process.env.HOST || '0.0.0.0'
+const port = config.port
+
+//Importar las variables de entorno locales
+//const variablesenv = require('./variables.env')
+require('dotenv').config({path: './variables.env'})
+
+console.log(process.env.DB_URL)
+
+//mongoose.connect(config.db, (err, res) => {
+mongoose.connect(process.env.DB_URL, (err, res) => {
     if (err) {
         return console.log(`Error al conectar a la base de datos: ${error}`)
     }
     console.log('Conexión a la base de datos establecida...')
 
-    app.listen(config.port, () => {
-        console.log(`API REST corriendo en http://localhost:${config.port}`)
+    //app.listen(config.port, () => {
+    app.listen(port, host, () => {
+        //console.log(`API REST corriendo en http://localhost:${config.port}`)
+        console.log('El servidor esta funcionando')
     })
 })
 
